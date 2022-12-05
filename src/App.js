a2d72c68fdd7a1236ccbfdca0b8a87499adb63dd
salauddin36 <salauddin36ayubi@gmail.com>
2022-12-05 20:22:00 +0530

    navigation menu modifies

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import Navbar from "./components/Navbar";
-import { Route, Switch } from "react-router-dom";
-import Product from "./components/Product";
-import Home from "./components/Home";
-import Admin from "./components/Admin";
+import "./style.css"
+import Navbar from "./components/Navbar"
+import Home from "./components/Home"
+import Admin from "./components/Admin"
+import Product from "./components/Products"
+
+import { Route, Switch } from 'react-router-dom';
+
 function App() {
   return (
     <div>
       <Navbar />
+
       <div>
         <Switch>
-          <Route path="/products" component={Product} />
-          <Route path="/admin" component={Admin} />
-          <Route path="/" component={Home} />
+          <Route path='/products' component={Product} />
+          <Route path='/admin' component={Admin} />
+          <Route path='/' component={Home} />
         </Switch>
       </div>
+
     </div>
   );
 }
